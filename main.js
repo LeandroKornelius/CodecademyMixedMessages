6e71cf915025dc79ed1c57f21a4b223985250cc1
leobelko <leobelko@gmail.com>
2024-04-11 13:28:19 +0000

    feat: randomIdx test to select index from array

---
+++ main.js
@@ -0,0 +1,15 @@
+const spidermanQuotes = [
+    "With great power comes great responsibility.",
+    "I believe there's a hero in all of us, that keeps us honest, gives us strength, makes us noble, and finally allows us to die with pride, even though sometimes we have to be steady, and give up the thing we want the most. Even our dreams.",
+    "The only thing standing between you and your goal is the story you keep telling yourself as to why you can't achieve it.",
+    "The more people I save, the more enemies I make.",
+    "I'm not a superhero. I'm just a kid who got bit by a spider.",
+    "One person can make a difference.",
+    "Sometimes, you have to take a leap of faith first. The trust part comes later.",
+    "It's not who I am underneath, but what I do that defines me.",
+    "The world needs heroes.",
+    "You're friendly neighborhood Spider-Man!",
+];
+
+const randomIdx = Math.floor(Math.random() * spidermanQuotes.length);
+console.log(randomIdx);
